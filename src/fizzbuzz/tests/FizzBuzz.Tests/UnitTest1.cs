6df8093151729b19d681c5ccbfc832b548dd4a12
Josh Soran <joshsoran@gmail.com>
2025-09-15 10:03:10 -0400

    feat(fizzbuzz): add solution, console app, and passing xUnit tests

---
+++ src/fizzbuzz/tests/FizzBuzz.Tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+public static class FizzBuzz
+{
+    public static string Of(int n)
+        => (n % 15 == 0) ? "FizzBuzz"
+         : (n % 3 == 0)  ? "Fizz"
+         : (n % 5 == 0)  ? "Buzz"
+         : n.ToString();
+}
+
+public class FizzBuzzTests
+{
+    [Theory]
+    [InlineData(1, "1")]
+    [InlineData(3, "Fizz")]
+    [InlineData(5, "Buzz")]
+    [InlineData(15, "FizzBuzz")]
+    public void FizzBuzz_Works(int n, string expected)
+    {
+        Assert.Equal(expected, FizzBuzz.Of(n));
+    }
+}
